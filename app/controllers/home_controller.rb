ddacb05adbfd87da8ebc1eefeb6be9b90394e2c0
koshin1024 <koshin1024@gmail.com>
2017-05-28 00:03:17 +0900

    画面遷移を追加

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+  def top
+  end
+
+  def select_top
+
+  end
+  
+  def explain_top
+  
+  end
+
+  def answer_a
+  
+  end
+  
+  def answer_b
+  
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  #トップページ
+  root 'home#top'
+
+  #説明のトップページ
+  get 'home/explain_top' => 'home#explain_top', as: 'explain'
+
+
+  #診断のトップページ
+  get 'home/select_top' => 'home#select_top', as: 'select'
+    
+  #診断の各ページ
+
+
+  #診断の最終回答ページ
+  get 'home/answer_a' => 'home#answer_a', as: 'answer_a'
+  get 'home/answer_b' => 'home#answer_b', as: 'answer_b'
+  
+  #このリンクは作業用として作成している。本番環境では利用しない。
+  get 'home/task' => 'home#task', as: 'task'
+
+  #scaffoldによる自動生成
   resources :notes
   #root to: 'notes#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
